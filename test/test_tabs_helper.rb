7975e6e15fe450eec137bd415573936bd6dc3941
Darkside73 <andrey.garbuz@gmail.com>
2013-07-06 20:22:33 +0300

    Add "id", "icon" and "class" options support for tab pane. Do not render pane if it has no content (arguable: need option for that)

--- test/test_tabs_helper.rb
+++ test/test_tabs_helper.rb
@@ -1,15 +1,24 @@
 require 'test_helper'
 
 class AccordionHelperTest < ActionView::TestCase
-  
+
   include BootstrapComponentsHelpers::TabsHelper
-  
+
   test "calling tabs method" do
-    tabs do |tabs|
-      tabs.pane 'My first pane' do
+    content = tabs do |tabs|
+      tabs.pane 'My first pane<br>', id: 'custom_id', icon: 'ok', class: 'fade' do
         'inside the pane'
       end
+      tabs.pane 'Empty pane' do
+        ''
+      end
     end
+    assert_match /href="#custom_id"/, content
+    assert_match /id="custom_id"/, content
+    assert_match /class="icon-ok"/, content
+    assert_match /class=".*fade.*"/, content
+    refute_match /<br>/, content
+    refute_match /Empty pane/, content
   end
 
 end
